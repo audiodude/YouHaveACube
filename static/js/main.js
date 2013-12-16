5ff9548700209ee2d42e26a4967a08c8a29fc37c
Travis Briggs <audiodude@gmail.com>
2013-12-16 08:35:53 -0800

    Untabify everything. Add description for outside the house, refactor some action commands and properly remove actions when user goes outside.

--- static/js/main.js
+++ static/js/main.js
@@ -1,184 +1,208 @@
 var cube = {
-	happy: 0
+  happy: 0
 };
 
+cube.DESCS = {
+  'house': '',
+  'outside': 'Outside it is an overcast fall day. There are a few straggling leaves in your yard, leftovers from raking. Your brother Lyle\'s <span class="object">car</span> is parked outside the yard\'s white picket fence.'
+}
+
+
 function happinessFor(view) {
-	view.happiness = '';
-	if (cube.happy >= 100) {
-		view.happiness = 'REALLY REALLY HAPPY';
-	} else if (cube.happy >=80) {
-		view.happiness = 'REALLY happy';
-	} else if (cube.happy >= 50) {
-		view.happiness = 'very happy';
+  view.happiness = '';
+  if (cube.happy >= 100) {
+    view.happiness = 'REALLY REALLY HAPPY';
+  } else if (cube.happy >=80) {
+    view.happiness = 'REALLY happy';
+  } else if (cube.happy >= 50) {
+    view.happiness = 'very happy';
   } else if (cube.happy >= 30) {
-		view.happiness = 'happy';
-	} else if (cube.happy < 0) {
-		view.happiness = 'grumpy';
-	}
+    view.happiness = 'happy';
+  } else if (cube.happy < 0) {
+    view.happiness = 'grumpy';
+  }
 }
 
 function leashFor(view) {
-	view.leash = '';
+  view.leash = '';
   if (cube.on_leash) {
-		view.leash = 'on a leash';
-  }	
+    view.leash = 'on a leash';
+  } 
 }
 
 function nameFor(view) {
   view.name = '';
-	if (cube.name) {
-		view.name = 'named "' + cube.name + '"';
+  if (cube.name) {
+    view.name = 'named "' + cube.name + '"';
   }
 }
 
 function addPrompt(value) {
-	view = {};
-	happinessFor(view);
-	leashFor(view);
-	nameFor(view);
+  view = {};
+  happinessFor(view);
+  leashFor(view);
+  nameFor(view);
 
-	value.prompt = Mustache.render(
-		'You have a {{happiness}}{{#happiness}} {{/happiness}}cube{{#name}} {{/name}}{{&name}}{{#leash}} {{/leash}}{{leash}}.', view);
+  value.prompt = Mustache.render(
+    'You have a {{happiness}}{{#happiness}} {{/happiness}}cube{{#name}} {{/name}}{{&name}}{{#leash}} {{/leash}}{{leash}}.', view);
 }
 
 cube.pet = function() {
-	cube.happy += 10;
+  cube.happy += 10;
 
-	value = {};
+  value = {};
   value.result = Mustache.render(
-		'You pet {{name}}{{^name}}the cube{{/name}}. It seems happy.',
-		{ 'name': cube.name }
-	);
-	addPrompt(value);
-	return value;
+    'You pet {{name}}{{^name}}the cube{{/name}}. It seems happy.',
+    { 'name': cube.name }
+  );
+  addPrompt(value);
+  return value;
 };
 
 cube.leash = function() {
-	var value = {};
-	if (cube.on_leash) {
-		value.result = Mustache.render(
-			'{{name}}{{^name}}Your cube{{/name}} is already on a leash.',
-			{ 'name': cube.name }
-		);
-	} else {
-		cube.on_leash = true;
-		cube.happy -= 25;
-		value.result = 'You put your cube on a leash. It doesn\'t seem happy ' + 
-			'about it.';
-		$('.word:contains("walk")').parent().fadeIn();
-	}
-	addPrompt(value);
-	return value;
+  var value = {};
+  if (cube.on_leash) {
+    value.result = Mustache.render(
+      '{{name}}{{^name}}Your cube{{/name}} is already on a leash.',
+      { 'name': cube.name }
+    );
+  } else {
+    cube.on_leash = true;
+    cube.happy -= 25;
+    value.result = 'You put your cube on a leash. It doesn\'t seem happy ' + 
+      'about it.';
+    appendAction('go for a <span class="word">walk</span>');
+  }
+  addPrompt(value);
+  return value;
 };
 
 cube.NAME_ADJ = [
-	'precious', 'darling', 'cutesy', 'cute', 'radical', 'hip', 'happenin', 'cool', 
-	'neat', 'gnarly', 'rockin', 'sweet'
+  'precious', 'darling', 'cutesy', 'cute', 'radical', 'hip', 'happenin', 'cool', 
+  'neat', 'gnarly', 'rockin', 'sweet'
 ];
 
 cube.write = function(callback) {
-	function processWriting() {
-		cube.name = $('#cube_name').val();
-		var name_adj = cube.NAME_ADJ[Math.floor(Math.random()*cube.NAME_ADJ.length)]
-		value = {
-			result: 'You named your cube "' + cube.name + '". What a ' + name_adj + 
-				' name!'
-		}
-		addPrompt(value)
-		callback(value)
-	}
+  function processWriting() {
+    cube.name = $('#cube_name').val();
+    var name_adj = cube.NAME_ADJ[Math.floor(Math.random()*cube.NAME_ADJ.length)]
+    value = {
+      result: 'You named your cube "' + cube.name + '". What a ' + name_adj + 
+        ' name!'
+    }
+    addPrompt(value)
+    callback(value)
+  }
 
   $('#result').html('You write <input id="cube_name" maxlength="12"> on the cube');
-	$('#cube_name').focus();
-	$('#cube_name').keyup(function(evt){
+  $('#cube_name').focus();
+  $('#cube_name').keyup(function(evt){
     if(evt.keyCode == 13){
-			processWriting();
+      processWriting();
     }
   });
 };
 
 cube.walk = function() {
-	value = {};
-	if (cube.happy < 0) {
-		value.result = Mustache.render(
-			'{{name}}{{^name}}Your cube{{/name}} is feeling too grumpy to go for a '+
-				'walk.',
-			{ 'name': cube.name }
-		);
-	} else {
-		value.result = Mustache.render(
-			'You grab your coat and head out the door with ' + 
-				'{{name}}{{^name}}your cube{{/name}}.',
-			{ 'name': cube.name }
-		);
-		window.setTimeout(function() {
-			animateGoDivs(setupWalk(animateBackDivs));
-		}, 1600);
-	}
-	addPrompt(value);
-	return value;
+  value = {};
+  if (cube.happy < 0) {
+    value.result = Mustache.render(
+      '{{name}}{{^name}}Your cube{{/name}} is feeling too grumpy to go for a '+
+        'walk.',
+      { 'name': cube.name }
+    );
+  } else {
+    value.result = Mustache.render(
+      'You grab your coat and head out the door with ' + 
+        '{{name}}{{^name}}your cube{{/name}}.',
+      { 'name': cube.name }
+    );
+    window.setTimeout(function() {
+      animateGoDivs(setupWalk.bind(undefined, animateBackDivs));
+    }, 1600);
+  }
+  addPrompt(value);
+  return value;
 }
 
 function animateGoDivs(callback) {
-	callback = callback || $.noop;
-	$('#result').animate({left: '100%'}, 100, function() {
-		$('#prompt').animate({left: '-100%'}, 100, function() {
-			$('#actions').animate({left: '100%'}, 100, function() {
-				callback();
-			});
-		});
-	});
+  callback = callback || $.noop;
+  $('#result').animate({left: '100%'}, 200, function() {
+    $('#description').animate({left: '100%'}, 200, function() {
+      $('#prompt').animate({left: '-100%'}, 200, function() {
+        $('#actions').animate({left: '100%'}, 200, function() {
+          callback();
+        });
+      });
+    });
+  });
 }
 
 function animateBackDivs(callback) {
-	callback = callback || $.noop;
-	$('#result').animate({left: '0'}, 100, function() {
-		$('#prompt').animate({left: '0'}, 100, function() {
-			$('#actions').animate({left: '0'}, 100, function() {
-				callback();
-			});
-		});
-	});
+  callback = callback || $.noop;
+  $('#result').animate({left: '0'}, 200, function() {
+    $('#description').animate({left: '0'}, 200, function() {
+      $('#prompt').animate({left: '0'}, 200, function() {
+        $('#actions').animate({left: '0'}, 200, function() {
+          callback();
+        });
+      });
+    });
+  });
+}
+
+function appendAction(action) {
+  var action = $('<span class="action" style="display: none">' + action + 
+                 '</span>')
+  $('#actions').append(action);
+  attachAction(undefined, action);
+  action.fadeIn();
+}
+
+function removeAction(word) {
+  $('#actions .action:contains("' + word + '")').remove();
 }
 
 function doWord(word) {
-	function callback(value) {
-		$('#result').text(value.result);
-		$('#prompt').text(value.prompt);
-	}
+  function callback(value) {
+    $('#result').text(value.result);
+    $('#prompt').text(value.prompt);
+  }
 
-	var value = cube[word](callback);
-	if (value) {
-		callback(value);
+  var value = cube[word](callback);
+  if (value) {
+    callback(value);
   }
 }
 
 function setupWalk(callback) {
-	window.setTimeout(callback, 1600);
+  $('#result').html('');
+  $('#description').html(cube.DESCS['outside']);
+  removeAction('walk');
+  removeAction('leash');
+  window.setTimeout(callback, 800);
 }
 
-function parseActions() {
-	$('#actions').children('.action').each(function(i, action) {
-		$(action).click(function(evt) {
-			var word = null;
-			var tar = $(evt.target);
-			if (tar.hasClass('word')) {
-				word = tar.text();
-			} else {
-				word = tar.children('.word').text();
-			}
-			if (!word) {
-				console.error('Could not get word: ', tar);
-				return;
-			}
-			$('#result').text('');
-			window.setTimeout(function() { doWord(word); }, 200);
-		});
-	});
+function attachAction(i, action) {
+  $(action).click(function(evt) {
+    var word = null;
+    var tar = $(evt.target);
+    if (tar.hasClass('word')) {
+      word = tar.text();
+    } else {
+      word = tar.children('.word').text();
+    }
+    if (!word) {
+      console.error('Could not get word: ', tar);
+      return;
+    }
+    $('#result').text('');
+    window.setTimeout(function() { doWord(word); }, 200);
+  });
 }
-	
+
 function init() {
-  parseActions();
+  $('#actions').children('.action').each(attachAction);
 }
 
 $(document).ready(init)
